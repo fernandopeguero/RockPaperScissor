ebf7a8dc714c99f36e84f7920b4ca8d52e87b4e1
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-09-23 11:05:38 -0400

    Add boilerplater for adding basic logic

---
+++ script.js
@@ -0,0 +1,29 @@
+/* write the rock paper scissor login 
+create one function for the machine and one function where you choose between the options
+then create a function to play the game.
+
+write a function getWinner that takes 2 int and return the winner 
+*/
+
+
+const choices = {
+    ROCK: 1,
+    PAPER: 2,
+    SCISSOR: 3
+}
+
+
+function getComputerChoice() {
+
+}
+
+
+function getHumanChoice() {
+
+}
+
+
+function getWinner() {
+
+
+}
