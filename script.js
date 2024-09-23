3273cf6d12ac5cd6d8047f0df7e71b2f681c7d9c
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-09-23 11:15:42 -0400

    ADD logic to getComputerChoice function for guessing a choice

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ write a function getWinner that takes 2 int and return the winner
 
 
 const choices = {
-    ROCK: 1,
-    PAPER: 2,
-    SCISSOR: 3
+    ROCK: "rock",
+    PAPER: "paper",
+    SCISSOR: "scissor"
 }
 
 
 function getComputerChoice() {
+    const keys = Object.keys(choices)
 
+    return choices[keys[Math.floor(keys.length * Math.random())]]
 }
 
 
