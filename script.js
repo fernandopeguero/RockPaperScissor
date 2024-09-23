b8d68bf01dcc6e6ca8ea178a9fb08d04d274896f
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-09-23 11:49:59 -0400

    Add logic for human player to select one of the options

--- script.js
+++ script.js
@@ -15,17 +15,37 @@ const choices = {
 
 function getComputerChoice() {
     const keys = Object.keys(choices)
-
     return choices[keys[Math.floor(keys.length * Math.random())]]
 }
 
 
 function getHumanChoice() {
 
+    let userInput = null
+
+    do {
+
+        userInput = prompt("1. Rock - 2. Paper - 3. Scissor.\n Please enter a number:");
+        userInput = Number(userInput); // Convert input to a number
+      } while (isNaN(userInput) || userInput < 1 || userInput > 3);
+
+
+      const keys = Object.keys(choices)
+
+    return choices[keys[userInput - 1]]
 }
 
 
-function getWinner() {
+function getWinner(humanChoice, computerChoice) {
 
+    if(humanChoice === "rock" && computerChoice !== "paper"){
+        console.log("Human Wins")
+    } else if (humanChoice === "paper" && computerChoice !== "scissor") {
+        console.log("Human Wins");
+    } else if (humanChoice === "scissor" && computerChoice !== "rock") {
+        console.log("Human Wins");
+    } else {
+        console.log("Computer Wins");
+    }
 
 }
