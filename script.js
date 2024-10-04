aa8cc09f92bfb6c998fb7092f4652417fdca589c
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-10-03 20:50:03 -0400

    Remove logic to play 5 games automatically

--- script.js
+++ script.js
@@ -5,78 +5,62 @@ then create a function to play the game.
 write a function getWinner that takes 2 int and return the winner 
 */
 
-
 const choices = {
     ROCK: "rock",
     PAPER: "paper",
-    SCISSOR: "scissor"
-}
-
+    SCISSOR: "scissor",
+};
 
 function getComputerChoice() {
-    const keys = Object.keys(choices)
-    return choices[keys[Math.floor(keys.length * Math.random())]]
+    const keys = Object.keys(choices);
+    return choices[keys[Math.floor(keys.length * Math.random())]];
 }
-
 
 function getHumanChoice() {
-
-    let userInput = null
+    let userInput = null;
 
     do {
-
-        userInput = prompt("1. Rock - 2. Paper - 3. Scissor.\n Please enter a number:");
+        userInput = prompt(
+            "1. Rock - 2. Paper - 3. Scissor.\n Please enter a number:"
+        );
         userInput = Number(userInput); // Convert input to a number
-      } while (isNaN(userInput) || userInput < 1 || userInput > 3);
+    } while (isNaN(userInput) || userInput < 1 || userInput > 3);
 
+    const keys = Object.keys(choices);
 
-      const keys = Object.keys(choices)
-
-    return choices[keys[userInput - 1]]
+    return choices[keys[userInput - 1]];
 }
 
-
 function playGame() {
-
-  
     let counter = 0;
     let humanScore = 0;
     let computerScore = 0;
 
-    while(counter < 5) {
-        
-        
-        const computerChoice = getComputerChoice();
-    
-        const humanChoice = getHumanChoice();
-    
-        getWinner(humanChoice, computerChoice);
-    
-        function getWinner(humanChoice, computerChoice) {
-    
-            if(humanChoice === "rock" && computerChoice !== "paper"){
-                humanScore++;
-            } else if (humanChoice === "paper" && computerChoice !== "scissor") {
-                humanScore++;
-            } else if (humanChoice === "scissor" && computerChoice !== "rock") {
-                humanScore++;
-            } else {
-                computerScore++;
-            }
-        
-        
+    const computerChoice = getComputerChoice();
+
+    const humanChoice = getHumanChoice();
+
+    getWinner(humanChoice, computerChoice);
+
+    function getWinner(humanChoice, computerChoice) {
+        if (humanChoice === "rock" && computerChoice !== "paper") {
+            humanScore++;
+        } else if (humanChoice === "paper" && computerChoice !== "scissor") {
+            humanScore++;
+        } else if (humanChoice === "scissor" && computerChoice !== "rock") {
+            humanScore++;
+        } else {
+            computerScore++;
         }
-
-        counter++;
     }
 
-    if(humanScore > computerScore) {
-        console.log("You Win! Hurray!!!")
-    } else if (humanScore < computerScore){
-        console.log("You Lose!, Try better next time you filthy human.")
+    counter++;
+
+    if (humanScore > computerScore) {
+        console.log("You Win! Hurray!!!");
+    } else if (humanScore < computerScore) {
+        console.log("You Lose!, Try better next time you filthy human.");
     } else {
-        console.log("Draw!!!, You are a formidable opponent.")
+        console.log("Draw!!!, You are a formidable opponent.");
     }
-
-
 }
